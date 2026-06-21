e6973e390e7f087b6f4b8456a7c48e97b7f7c82b
Daria <145852365+Param-param-pam@users.noreply.github.com>
2026-06-21 19:04:58 +0300

    init upgrade1

--- app.py
+++ app.py
@@ -8,55 +8,76 @@ st.set_page_config(page_title="Sleep Dashboard", layout="wide")
 # Данные
 
 data = [
-    ['09.06','Юля',8.5,8.5],
-    ['09.06','Даша',7.5,8],
-    ['10.06','Юля',5.5,6],
-    ['10.06','Даша',7,7.5],
-    ['11.06','Юля',6.7,6.7],
-    ['11.06','Даша',6.5,6.8],
-    ['12.06','Юля',7.5,7.5],
-    ['12.06','Даша',8,8],
-    ['13.06','Юля',4,4.5],
-    ['13.06','Даша',4,4.5],
-    ['14.06','Юля',9,9],
-    ['14.06','Даша',8.5,8.5],
-    ['15.06','Юля',4.5,4.5],
-    ['15.06','Даша',8,7.5],
-    ['16.06','Юля',8,8.5],
-    ['16.06','Даша',6,6],
-    ['17.06','Юля',5.5,5.5],
-    ['17.06','Даша',6.5,7],
-    ['18.06','Юля',7,7],
-    ['18.06','Даша',7,7],
-    ['19.06','Юля',2.5,2.5],
-    ['19.06','Даша',7,7],
-    ['20.06','Юля',9,9],
-    ['20.06','Даша',7.5,7.5],
-    ['21.06','Юля',7,7],
-    ['21.06','Даша',9,9.5]
+    ['09.06.2026','Юля',8.5,8.5],
+    ['09.06.2026','Даша',7.5,8],
+    ['10.06.2026','Юля',5.5,6],
+    ['10.06.2026','Даша',7,7.5],
+    ['11.06.2026','Юля',6.7,6.7],
+    ['11.06.2026','Даша',6.5,6.8],
+    ['12.06.2026','Юля',7.5,7.5],
+    ['12.06.2026','Даша',8,8],
+    ['13.06.2026','Юля',4,4.5],
+    ['13.06.2026','Даша',4,4.5],
+    ['14.06.2026','Юля',9,9],
+    ['14.06.2026','Даша',8.5,8.5],
+    ['15.06.2026','Юля',4.5,4.5],
+    ['15.06.2026','Даша',8,7.5],
+    ['16.06.2026','Юля',8,8.5],
+    ['16.06.2026','Даша',6,6],
+    ['17.06.2026','Юля',5.5,5.5],
+    ['17.06.2026','Даша',6.5,7],
+    ['18.06.2026','Юля',7,7],
+    ['18.06.2026','Даша',7,7],
+    ['19.06.2026','Юля',2.5,2.5],
+    ['19.06.2026','Даша',7,7],
+    ['20.06.2026','Юля',9,9],
+    ['20.06.2026','Даша',7.5,7.5],
+    ['21.06.2026','Юля',7,7],
+    ['21.06.2026','Даша',9,9.5]
     ]
 
 df = pd.DataFrame(data, columns=['date','name','min','max'])
 df['sleep'] = (df['min']+df['max'])/2
-df['date']=pd.to_datetime(df['date'],format = '%d.%m')
+df['date']=pd.to_datetime(df['date'],format = '%d.%m.%Y')
+
+st.sidebar.header('Фильтры')
+selected_person = st.sidebar.selectbox("Кто", ['Все','Юля','Даша'])
+
+if selected_person =='Все':
+    filtered_df = df.copy()
+else: filtered_df=df[df['name']== selected_person]
 
 st.title("😴 Sleep Dashboard")
 
 # KPI
-col1, col2, col3 = st.columns(3)
-avg_sleep = df.groupby('name')['sleep'].mean()
-std_sleep = df.groupby('name')['sleep'].std()
-deficit = df.assign(deficit=df['sleep']-7).groupby('name')['deficit'].sum()
+col1, col2, col3, col4 = st.columns(4)
+avg_sleep = filtered_df['sleep'].mean()
+best__sleep = filtered_df['sleep'].max()
+worst_sleep = filtered_df['sleep'].min()
+#std_sleep = df.groupby('name')['sleep'].std()
+#deficit = df.assign(deficit=df['sleep']-7).groupby('name')['deficit'].sum()
+sleep_deficit = (filtered_df['sleep'] -7).sum()
 
-st.metric("Средний сон Юля", f"{avg_sleep['Юля']:.2f}")
-st.metric("Средний сон Даша", f"{avg_sleep['Даша']:.2f}")
+#st.metric("Средний сон Юля", f"{avg_sleep['Юля']:.2f}")
+#st.metric("Средний сон Даша", f"{avg_sleep['Даша']:.2f}")
+with col1:
+    st.metric("Средний сон",f"{avg_sleep:.2f} ч")
+
+with col2:
+    st.metric("Лучшая ночь",f"{best__sleep:.1f} ч")
+
+with col3:
+    st.metric("Худшая ночь",f"{worst_sleep:.1f} ч")
+
+with col4:
+    st.metric("Дефицит сна",f"{sleep_deficit:.1f} ч")
 
 st.write("___")
 
 # График тренда
 st.subheader("📈 Тренд сна")
 
-fig = px.line(df, x='date', y='sleep', color = 'name', markers=True)
+fig = px.line(filtered_df, x='date', y='sleep', color = 'name', markers=True)
 fig.add_hline(y=7)
 
 st.plotly_chart(fig, width='stretch')
@@ -64,7 +85,7 @@ st.plotly_chart(fig, width='stretch')
 # Календарь сна (heatmap)
 st.subheader("🗓️ Календарь сна")
 
-fig2 = px.density_heatmap(df, x = 'date', y ='name', z = 'sleep', color_continuous_scale="RdYlGn")
+fig2 = px.density_heatmap(filtered_df, x = 'date', y ='name', z = 'sleep', color_continuous_scale="RdYlGn")
 
 st.plotly_chart(fig2, width='stretch')
 
@@ -98,4 +119,4 @@ st.plotly_chart(fig3, width='stretch')
 
 # Таблица
 st.subheader("📋 Табличка")
-st.dataframe(df)
+st.dataframe(filtered_df)
