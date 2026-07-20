97135e70705608ec979502238a11d380bcf87d32
Daria <145852365+Param-param-pam@users.noreply.github.com>
2026-07-21 01:04:05 +0300

    init change

--- app.py
+++ app.py
@@ -152,7 +152,7 @@ avg_sleep = filtered_df['sleep'].mean()
 best__sleep = filtered_df['sleep'].max()
 worst_sleep = filtered_df['sleep'].min()
 sleep_deficit = (filtered_df['sleep'] -7.5).sum()
-best ='19 июля'
+best_data ='19 июля'
 
 #Sleep_score = 100
 avg_deficit = abs(filtered_df['sleep']-7.5).mean()
@@ -177,7 +177,7 @@ with col4:
     st.metric("✳ Качество сна",f"{sleep_score:.0f}/100")
 
 with col6:
-    st.metric("💝 Самая лучшая ночь ",f"{best}")
+    st.metric("💝 Самая лучшая ночь ", best_data.strftime("%d.%m"))
 
 st.write("___")
 
